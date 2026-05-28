03c7b501ca4f7f5767633f25100b69d7ad3b4a0d
ivasilyev <u0412u0418u042e@gmail.com>
2026-05-28 18:13:23 +0300

    Added logger package

---
+++ log.py
@@ -0,0 +1,33 @@
+import logging
+import threading
+from sys import stdout
+
+LOGGING_FORMAT = "%(asctime)s.%(msecs)06d [%(filename)s %(name)s] %(levelname)s %(funcName)s(%(lineno)s) - %(message)s"
+DATE_FORMAT = "%Y.%m.%d %H:%M:%S"
+
+
+class Log:
+    _instance = None
+    _lock = threading.Lock()  # Shared lock across all instances
+
+    def __new__(cls):
+        # First check (unlocked) for high performance after initialization
+        if cls._instance is None:
+            # Synchronize threads
+            with cls._lock:
+                # Second check (locked) to prevent race conditions
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        # _LOG = get_logger()
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(LOGGING_FORMAT)
+        stdout_handler = logging.StreamHandler(stdout)
+        stdout_handler.setFormatter(formatter)
+        self.logger.addHandler(stdout_handler)
+
+
+log = Log().logger
