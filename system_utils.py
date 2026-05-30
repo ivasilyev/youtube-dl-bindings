907009fdb9aac3d9aadcd207937102c243ee6122
ivasilyev <u0412u0418u042e@gmail.com>
2026-05-30 11:57:07 +0300

    Neglected a default program running timeout

--- system_utils.py
+++ system_utils.py
@@ -10,7 +10,7 @@ def is_windows_os() -> bool:
     return os.name == "nt"
 
 
-def run_external_program(command: str, timeout: int = 30) -> Tuple[str, str, int]:
+def run_external_program(command: str, timeout: int = None) -> Tuple[str, str, int]:
     """
     Executes an external program and returns its stdout, stderr, and exit code.
 
@@ -51,9 +51,7 @@ def run_external_program(command: str, timeout: int = 30) -> Tuple[str, str, int
 
 
 def run_external_program_test():
-    print(run_external_program('echo "aaaa bbb ccc'))
-    # print(subprocess.getoutput('C:/Windows/system32/cmd.exe -c echo "aaaa bbb ccc"'))
-    # print(subprocess.getoutput('echo "aaaa bbb ccc"'))
+    print(run_external_program('echo "aaaa bbb ccc"'))
 
 
 if __name__ == '__main__':
