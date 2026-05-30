4de56ed7afe0e451a753f72a9f473dc0c9c542ba
ivasilyev <u0412u0418u042e@gmail.com>
2026-05-30 18:51:30 +0300

    Added logging to single video downloader

--- single_downloader.py
+++ single_downloader.py
@@ -7,6 +7,7 @@ _cfg = ConfigurationManager()
 
 
 def single_download(url: str, directory: str):
+    log.info(f"Download video URL '{url}' into directory '{directory}'")
     template = _cfg.get_single_download_template()
     command = sanitize_command(render_template(template_string=template, values_dict=dict(url=url, directory=directory)))
     run_external_program(command=command)
