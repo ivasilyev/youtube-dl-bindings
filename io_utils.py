db9a56515f1124b4c57f03171922dca2ddb8df14
ivasilyev <u0412u0418u042e@gmail.com>
2026-06-07 13:28:17 +0300

    Added method to export string into file

--- io_utils.py
+++ io_utils.py
@@ -2,14 +2,23 @@ import re
 from typing import List
 
 from constants import ENCODING_UTF8
+from log import log
 
 
 def load_string(file: str) -> str:
     with open(file, "r", encoding=ENCODING_UTF8) as file:
         s = file.read()
+    log.debug(f"Loaded: '{file}'")
     return s
 
 
 def load_lines(file: str) -> List[str]:
     s = load_string(file)
     return re.split("[\n\r]", s)
+
+
+def dump_string(string: str, file: str) -> None:
+    with open(file, "w", encoding=ENCODING_UTF8) as f:
+        f.write(string)
+        f.close()
+    log.debug(f"Saved: '{file}'")
